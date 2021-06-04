40cd7eb267fe3d380d4db52286ef379f4d3c797f
margish-mpc <margish@mpconsultants.org>
2021-06-04 15:47:01 +0530

    replace barcode data in script
    Pulse ID : 1255322133

--- pill/init.py
+++ pill/init.py
@@ -5,12 +5,12 @@ def main():
     print('Zebra Printer IP Addr for 1"x1": %s' % (sys.argv[1]))
     print('Model No: %s' % (sys.argv[2]))
     print('S/N : %s' % (sys.argv[3]))
-    print('FCC ID : %s' % (sys.argv[4]))
+    print('Barcode data : %s' % (sys.argv[4]))
     ip_1x1 = sys.argv[1]
     model = sys.argv[2]
     serial_no = sys.argv[3]
-    fcc_id = sys.argv[4]
-    mpc_zebra.print_label_prod_1x1(ip_1x1, model,serial_no, fcc_id)
+    barcode_data = sys.argv[4]
+    mpc_zebra.print_label_prod_1x1(ip_1x1, model,serial_no, barcode_data)
 
 if __name__ == '__main__':
     main()
--- pill/mpc_zebra.py
+++ pill/mpc_zebra.py
@@ -4,16 +4,16 @@ PRINTER_PORT = 9100
 
 MODEL = "QS-CO-001"
 SERIAL_NO = "MMYY-abc123"
-FCC_ID = "123456789"
+BARCODE_DATA = "123456789"
 
-def print_label_prod_1x1(ip, model, serial_no, fcc_id):
+def print_label_prod_1x1(ip, model, serial_no, barcode_data):
     label_file = open("prod_label_1x1.zpl")
     zpl_print_str = label_file.read()
     label_file.close()
 
     zpl_print_str = zpl_print_str.replace(MODEL, model)
     zpl_print_str = zpl_print_str.replace(SERIAL_NO, serial_no)
-    zpl_print_str = zpl_print_str.replace(FCC_ID, fcc_id)
+    zpl_print_str = zpl_print_str.replace(BARCODE_DATA, barcode_data)
     print(zpl_print_str)
     return
     new_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
